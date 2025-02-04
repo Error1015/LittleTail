b3ce74623c8bb0656991c620e13eeac1e99395b6
Error1015 <liangliang_1015@qq.com>
2025-02-04 21:17:05 +0800

    fix

---
+++ src/main/java/org/error1015/littletail/Config.java
@@ -0,0 +1,38 @@
+package org.error1015.littletail;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Collections;
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = Littletail.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    private static final ForgeConfigSpec.BooleanValue enable = BUILDER.comment("if true, it will enable to all player.if false, it will enable to list player", "如果为true,小尾巴将对所有玩家生效,如果为false,将只会对玩家名单中生效")
+            .define("enable", true);
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> playerCatList = BUILDER.comment("if player name in it, this player's chat will append tail.", "如果玩家名字在里面,这个玩家的发言将会附加小尾巴")
+            .defineList("catPlayerNameList", Collections.emptyList(), o -> o instanceof String);
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> PlayerUUIDList = BUILDER.comment("if player's uuid in it, this player's chat will append tail.", "如果玩家的UUID在里面,这个玩家的发言将会附加小尾巴").defineList("catPlayerUUIDList", Collections.emptyList(), o -> o instanceof String);
+    private static final ForgeConfigSpec.ConfigValue<? extends String> tail = BUILDER.comment("tail pattern", "小尾巴样式")
+            .define("tail", "喵~", o -> o instanceof String);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    public static List<? extends String> getPlayerCatList() {
+        return playerCatList.get();
+    }
+
+    public static List<? extends String> getPlayerUUIDList() {
+        return PlayerUUIDList.get();
+    }
+
+    public static String getTail() {
+        return tail.get();
+    }
+
+    public static boolean isEnable() {
+        return enable.get();
+    }
+}
---
+++ src/main/java/org/error1015/littletail/Littletail.java
@@ -0,0 +1,48 @@
+package org.error1015.littletail;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Mod(Littletail.MODID)
+@Mod.EventBusSubscriber(modid = Littletail.MODID)
+public class Littletail {
+    public static final String MODID = "littletail";
+    // 创建Set集合过滤重复项
+    private static final Set<String> CACHE_PLAYERS = new HashSet<>();
+    private static final Set<String> CACHE_PLAYERS_UUID = new HashSet<>();
+
+    public Littletail() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    @SubscribeEvent
+    public static void onServerChat(ServerChatEvent event) {
+        Player player = event.getPlayer();
+        String rawMessage = event.getRawText();
+        // 如果信息开头是"/" 直接退出不做处理
+        if (rawMessage.startsWith("/")) return;
+        // 如果信息开头是"notail" 把字符串切割前面6位 最后的内容就是信息 "notail a" -> "a"
+        if (rawMessage.startsWith("notail")) {
+            event.setMessage(Component.literal(rawMessage.substring(7)));
+            return;
+        }
+        // 如果CACHE_PLAYERS为空 添加PlayerCatList的内容
+        if (CACHE_PLAYERS.isEmpty()) CACHE_PLAYERS.addAll(Config.getPlayerCatList());
+        if (CACHE_PLAYERS_UUID.isEmpty()) CACHE_PLAYERS_UUID.addAll(Config.getPlayerUUIDList());
+        // 如果集合中包含了玩家的名字或者是UUID 又或者默认是生效的 那么他发送的消息将添加上小尾巴
+        if (CACHE_PLAYERS.contains(player.getName().getString())
+                || CACHE_PLAYERS_UUID.contains(player.getUUID().toString())
+                || Config.isEnable()) {
+            Component finalMessage = event.getMessage().copy().append(Config.getTail());
+            event.setMessage(finalMessage);
+        }
+    }
+}
